9a021adc9f4f4f247429ad2843cc1583002b75f3
SoonQchernobyl <113972823+SoonQchernobyl@users.noreply.github.com>
2024-07-11 18:36:36 +0900

    07011 final commit

--- portfolio/src/components/HelloWorld.vue
+++ portfolio/src/components/HelloWorld.vue
@@ -1,20 +1,25 @@
 <template>
-  <div
-    class="w-screen h-screen bg-indigo-900 flex justify-center items-center p-10"
-  >
+  <div class="w-screen h-screen flex justify-center items-center p-10" id="app">
     <div
-      class="w-full h-full bg-black shadow-lg relative overflow-hidden cmd-background"
+      class="w-full h-full shadow-lg relative overflow-hidden"
+      id="cmd-background"
     >
       <!-- Container for Vertical Menu and Content -->
       <div class="flex h-full">
         <!-- Vertical Menu -->
-        <ol class="menu text-white p-4">
-          <li class="mb-4 cursor-pointer" @click="goToSlide(0)">Home</li>
-          <li class="mb-4 cursor-pointer" @click="goToSlide(1)">
-            Development Philosophy
+        <ol class="menu text-black p-4">
+          <li class="mb-4 cursor-pointer font-extrabold" @click="goToSlide(0)">
+            Home
           </li>
-          <li class="mb-4 cursor-pointer" @click="goToSlide(2)">Projects</li>
-          <li class="mb-4 cursor-pointer" @click="goToSlide(3)">Contact</li>
+          <li class="mb-4 cursor-pointer font-extrabold" @click="goToSlide(1)">
+            Philosophy
+          </li>
+          <li class="mb-4 cursor-pointer font-extrabold" @click="goToSlide(2)">
+            Projects
+          </li>
+          <li class="mb-4 cursor-pointer font-extrabold" @click="goToSlide(3)">
+            Contact
+          </li>
         </ol>
 
         <!-- Swiper Container -->
@@ -44,19 +49,26 @@
         >
           <CustomSwiperSlide>
             <section
-              class="flex justify-between bg-black bg-opacity-70 text-white p-10 m-10 h-[calc(100%-5rem)] border-thick"
+              class="flex justify-between bg-black bg-opacity-70 text-#1F2937 p-10 m-10 h-[calc(100%-5rem)] border-thick"
             >
               <div class="leading-relaxed">
-                <h1 class="text-6xl leading-relaxed scrawl-font">
-                  Hi, I'm Cheru!
-                </h1>
-                <h3>Home</h3>
+                <div class="flex justify-between items-center flex-col">
+                  <h1 class="text-6xl leading-relaxed scrawl-font">
+                    Hellow, I'm Cheru!
+                  </h1>
+                  <p class="text-2xl">
+                    A Result-Oriented Web Developer building and managing
+                    Websites and Web Applications that leads to the success of
+                    the overall product
+                  </p>
+                  <a href="./#projects" class="btn btn--bg">Projects</a>
+                </div>
               </div>
             </section>
           </CustomSwiperSlide>
           <CustomSwiperSlide>
             <section
-              class="flex justify-between bg-black bg-opacity-70 text-white p-10 m-10 h-[calc(100%-5rem)] border-thick"
+              class="flex justify-between bg-black bg-opacity-70 text-#1F2937 p-10 m-10 h-[calc(100%-5rem)] border-thick"
             >
               <div class="leading-relaxed">
                 <h1 class="text-6xl leading-relaxed scrawl-font">Who am i?</h1>
@@ -66,7 +78,7 @@
           </CustomSwiperSlide>
           <CustomSwiperSlide>
             <section
-              class="flex flex-col justify-start bg-black bg-opacity-70 text-white p-10 m-10 h-[calc(100%-5rem)] border-thick"
+              class="flex flex-col justify-start bg-black bg-opacity-70 text-#1F2937 p-10 m-10 h-[calc(100%-5rem)] border-thick"
             >
               <div class="leading-relaxed mb-10">
                 <h1 class="text-6xl leading-relaxed scrawl-font">I Made ...</h1>
@@ -106,7 +118,7 @@
           </CustomSwiperSlide>
           <CustomSwiperSlide>
             <section
-              class="flex justify-between bg-black bg-opacity-70 text-white p-10 m-10 h-[calc(100%-5rem)] border-thick"
+              class="flex justify-between bg-black bg-opacity-70 text-#1F2937 p-10 m-10 h-[calc(100%-5rem)] border-thick"
             >
               <div class="leading-relaxed">
                 <h1 class="text-6xl leading-relaxed scrawl-font">
@@ -144,10 +156,12 @@ export default {
   font-family: "Permanent Marker", cursive;
 }
 
-.cmd-background {
+#cmd-background {
   position: relative;
   background-size: cover;
   background-repeat: no-repeat;
+  background-color: #ffffff;
+  border: black 5px solid;
 }
 
 .menu {
@@ -155,15 +169,56 @@ export default {
 }
 
 .border-thick {
-  border: 5px solid #533d7a; /* Thicker border */
+  border: 3px solid black; /* Thicker border */
+  border-radius: 15px;
 }
 
-.section-bg {
+#app {
+  background-color: #ffffff;
+}
+
+section {
   background: linear-gradient(
       to right,
       rgba(245, 245, 245, 0.8),
       rgba(245, 245, 245, 0.8)
     ),
     url("https://www.rammaheshwari.com/assets/svg/common-bg.svg");
+  background-size: cover;
+  background-position: center;
+}
+
+li {
+  line-height: 2;
+  --pad: max(20px, 4vmin);
+  --fs-text: 15px;
+  --c-bg: hsl(0, 0%, 5%);
+  --c-text: hsl(0, 0%, 95%);
+  --vw: 8.91px;
+  --vh: 9.19px;
+  --vmax: 9.19px;
+  --vmin: 8.91px;
+  font-weight: 600;
+  font-size: var(--fs-text);
+  font-feature-settings: "palt";
+  text-size-adjust: none;
+  -webkit-font-smoothing: antialiased;
+  font-family: "Neue Montreal Mono";
+  white-space: nowrap;
+  list-style: none;
+  box-sizing: inherit;
+  user-select: inherit;
+  -webkit-user-drag: inherit;
+  -webkit-tap-highlight-color: inherit;
+  backface-visibility: inherit;
+  margin: 0;
+  padding: 0;
+  color: inherit;
+  text-decoration: none;
+  position: relative;
+  transition-property: opacity;
+  transition-duration: 0.4s;
+  transition-timing-function: cubic-bezier(0.1, 0.4, 0.2, 1);
 }
 </style>
+style
