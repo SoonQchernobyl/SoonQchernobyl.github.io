ef64d29e26d2d8953d98f8c75e3beebce3dbe16a
SoonQchernobyl <113972823+SoonQchernobyl@users.noreply.github.com>
2024-07-12 14:50:33 +0900

    css configuration

--- portfolio/src/components/HelloWorld.vue
+++ portfolio/src/components/HelloWorld.vue
@@ -7,18 +7,53 @@
       <!-- Container for Vertical Menu and Content -->
       <div class="flex h-full">
         <!-- Vertical Menu -->
-        <ol class="menu text-black p-4">
-          <li class="mb-4 cursor-pointer font-extrabold" @click="goToSlide(0)">
-            Home
+        <ol class="menu text-black p-4 pt-8">
+          <li @click="goToSlide(0)">
+            <a class="group flex items-center py-3 active" href="#about"
+              ><span
+                class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
+              ></span
+              ><span
+                class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"
+                >About</span
+              ></a
+            >
           </li>
-          <li class="mb-4 cursor-pointer font-extrabold" @click="goToSlide(1)">
-            Philosophy
+
+          <li @click="goToSlide(1)">
+            <a class="group flex items-center py-3 active" href="#about"
+              ><span
+                class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
+              ></span
+              ><span
+                class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"
+                >Philosophy</span
+              ></a
+            >
           </li>
-          <li class="mb-4 cursor-pointer font-extrabold" @click="goToSlide(2)">
-            Projects
+
+          <li @click="goToSlide(2)">
+            <a class="group flex items-center py-3 active" href="#about"
+              ><span
+                class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
+              ></span
+              ><span
+                class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"
+                >Projects</span
+              ></a
+            >
           </li>
-          <li class="mb-4 cursor-pointer font-extrabold" @click="goToSlide(3)">
-            Contact
+
+          <li @click="goToSlide(3)">
+            <a class="group flex items-center py-3 active" href="#about"
+              ><span
+                class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
+              ></span
+              ><span
+                class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"
+                >Contact</span
+              ></a
+            >
           </li>
         </ol>
 
@@ -51,28 +86,32 @@
             <section
               class="flex justify-between bg-black bg-opacity-70 text-#1F2937 p-10 m-10 h-[calc(100%-5rem)] border-thick"
             >
-              <div class="leading-relaxed">
-                <div class="flex justify-between items-center flex-col">
-                  <h1 class="text-6xl leading-relaxed scrawl-font">
+              <div class="flex justify-center leading-relaxed">
+                <div class="flex justify-start items-center flex-col">
+                  <h1 class="text-6xl text-white leading-relaxed scrawl-font">
                     Hellow, I'm Cheru!
                   </h1>
-                  <p class="text-2xl">
+                  <p class="text-2xl text-white text-center">
                     A Result-Oriented Web Developer building and managing
                     Websites and Web Applications that leads to the success of
                     the overall product
                   </p>
-                  <a href="./#projects" class="btn btn--bg">Projects</a>
+                  <button class="btn mt-auto" @click="goToSlide(2)">
+                    Projects
+                  </button>
                 </div>
               </div>
             </section>
           </CustomSwiperSlide>
           <CustomSwiperSlide>
             <section
-              class="flex justify-between bg-black bg-opacity-70 text-#1F2937 p-10 m-10 h-[calc(100%-5rem)] border-thick"
+              class="flex justify-between bg-slate-600 bg-opacity-70 text-#1F2937 p-10 m-10 h-[calc(100%-5rem)] border-thick"
             >
               <div class="leading-relaxed">
-                <h1 class="text-6xl leading-relaxed scrawl-font">Who am i?</h1>
-                <h3>Development Philosophy</h3>
+                <h1 class="text-6xl text-white leading-relaxed scrawl-font">Who am i?</h1>
+                <p class="text-2xl text-white ">
+                  Development Philosophy
+                  </p>
               </div>
             </section>
           </CustomSwiperSlide>
@@ -81,8 +120,10 @@
               class="flex flex-col justify-start bg-black bg-opacity-70 text-#1F2937 p-10 m-10 h-[calc(100%-5rem)] border-thick"
             >
               <div class="leading-relaxed mb-10">
-                <h1 class="text-6xl leading-relaxed scrawl-font">I Made ...</h1>
-                <h3>Projects</h3>
+                <h1 class="text-6xl text-white leading-relaxed scrawl-font">I Made ...</h1>
+                <p class="text-2xl text-white ">
+                  Projects
+                  </p>
               </div>
               <div class="grid grid-cols-2 gap-4 w-full">
                 <div class="bg-gray-800 p-4 rounded border border-white">
@@ -121,10 +162,12 @@
               class="flex justify-between bg-black bg-opacity-70 text-#1F2937 p-10 m-10 h-[calc(100%-5rem)] border-thick"
             >
               <div class="leading-relaxed">
-                <h1 class="text-6xl leading-relaxed scrawl-font">
+                <h1 class="text-6xl text-white leading-relaxed scrawl-font">
                   Contact right now!
                 </h1>
-                <h3>Contact</h3>
+                <p class="text-2xl text-white ">
+                  Contact
+                  </p>
               </div>
             </section>
           </CustomSwiperSlide>
@@ -156,12 +199,16 @@ export default {
   font-family: "Permanent Marker", cursive;
 }
 
+#app {
+  background-color: #1f2229;
+}
+
 #cmd-background {
   position: relative;
   background-size: cover;
   background-repeat: no-repeat;
-  background-color: #ffffff;
-  border: black 5px solid;
+  background-color: #1f2229;
+  border: #60dbfa 2px solid;
 }
 
 .menu {
@@ -169,56 +216,45 @@ export default {
 }
 
 .border-thick {
-  border: 3px solid black; /* Thicker border */
+  border: 2px solid #60dbfa; /* Thicker border */
   border-radius: 15px;
 }
 
-#app {
-  background-color: #ffffff;
-}
-
 section {
-  background: linear-gradient(
-      to right,
-      rgba(245, 245, 245, 0.8),
-      rgba(245, 245, 245, 0.8)
-    ),
-    url("https://www.rammaheshwari.com/assets/svg/common-bg.svg");
-  background-size: cover;
+  background-color: #1f2229;
+  background: linear-gradient(90deg, #1f2229, #60dbfa);
+  background-size: 100% 20%;
   background-position: center;
+  background-repeat: no-repeat;
+  background-position: bottom right;
 }
 
 li {
   line-height: 2;
-  --pad: max(20px, 4vmin);
   --fs-text: 15px;
-  --c-bg: hsl(0, 0%, 5%);
-  --c-text: hsl(0, 0%, 95%);
-  --vw: 8.91px;
-  --vh: 9.19px;
-  --vmax: 9.19px;
-  --vmin: 8.91px;
   font-weight: 600;
   font-size: var(--fs-text);
-  font-feature-settings: "palt";
-  text-size-adjust: none;
-  -webkit-font-smoothing: antialiased;
   font-family: "Neue Montreal Mono";
-  white-space: nowrap;
-  list-style: none;
   box-sizing: inherit;
-  user-select: inherit;
-  -webkit-user-drag: inherit;
-  -webkit-tap-highlight-color: inherit;
-  backface-visibility: inherit;
-  margin: 0;
-  padding: 0;
-  color: inherit;
-  text-decoration: none;
-  position: relative;
   transition-property: opacity;
   transition-duration: 0.4s;
   transition-timing-function: cubic-bezier(0.1, 0.4, 0.2, 1);
 }
+
+.btn {
+  background: #1f2937;
+  color: #fff;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: 700;
+  border-radius: 5px;
+  box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.15);
+  transition: transform 0.3s;
+  padding: 0.5rem 4rem;
+  font-size: 2rem;
+}
+
+.btn:hover {
+  transform: translateY(-3px);
+}
 </style>
-style
