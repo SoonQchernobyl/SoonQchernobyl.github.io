46bef749be17829248702defa2883dadba9d6b1b
SoonQchernobyl <113972823+SoonQchernobyl@users.noreply.github.com>
2024-08-13 10:28:33 +0900

    0813_1

--- portfolio/vue.config.js
+++ portfolio/vue.config.js
@@ -1,6 +1,3 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/SoonQchernobyl.github.io/" : "/",
-});
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "/",
+};
